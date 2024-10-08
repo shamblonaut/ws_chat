b98adbeca59b67d57a8be1a7eccf32ae2c8ce6f6
Shamblonaut <shaheembn@gmail.com>
2024-10-08 19:08:34 +0000

    chore: setup production deploys

---
+++ config/prod.exs
@@ -0,0 +1,5 @@
+config :ws_chat, WsChat.Endpoint,
+  http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
