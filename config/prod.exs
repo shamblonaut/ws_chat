210c74d29efd8e239346a7e12b47a64869379262
Shamblonaut <shaheembn@gmail.com>
2024-10-10 18:11:16 +0000

    fix?: gigalixir database problems

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,6 @@
 config :ws_chat, WsChat.Endpoint,
   # Possibly not needed, but doesn't hurt
   http: [port: {:system, "PORT"}],
-
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   server: true
@@ -9,6 +8,7 @@ config :ws_chat, WsChat.Endpoint,
 config :ws_chat, WsChat.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
+  database: "",
   ssl: true,
 
   # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas
